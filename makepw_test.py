b81b0efe90d60fce33f3c49c89881c9baa779441
Eric Hopper <hopper@omnifarious.org>
2018-11-01 14:48:52 -0700

    Change name of test parameter list to be more descriptive.

--- makepw_test.py
+++ makepw_test.py
@@ -7,13 +7,13 @@ def test_has_main():
 def test_has_pbkdf2():
     assert(getattr(makepw, 'pbkdf2'))
 
-pwgen_results = [
+hasher_results = [
     (makepw.pbkdf2, b'\x8a\x1e\xd4 XPK\xea\xcc\xec[>n"\xdc\x05V\xfa\x14\x7f\x95\x1b\x10H\x0f[\xc6]\xc496\x00'),
     (makepw.not_pbkdf2, b'\xa7\xdb\x1bH\xc6\xddW\xc3g\x12]\xc2B\xb2\x08\x11\x88[\xcd-WF\xc9\xb4\xfb\xd4\x94\x90\x07\xddi\x89')
     ]
 
-@pytest.mark.parametrize("hasher,expected", pwgen_results)
-def test_hashers(hasher, expected):
+@pytest.mark.parametrize("hasher,expected", hasher_results)
+def test_hasher_results(hasher, expected):
     h = hasher(b'fred', b'barney', 500)
     assert(isinstance(h, bytes))
     assert(h == expected)
